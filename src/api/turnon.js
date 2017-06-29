e0ce2b3914cb1be47a8d08285ff4ec5d2ed34331
matheus.godinho <matheus.godinho@zrp.com.br>
2017-06-29 01:55:10 -0300

    Adicionando lógica para adicionar açucar no reator

--- src/api/turnon.js
+++ src/api/turnon.js
@@ -8,18 +8,25 @@ export default ({ config, db }) => {
     try {
       var port = body.port
       var mode = body.mode
+      var brix = body.brix
+      var rotation = 50
+      var total = brix * 211
+      var rotations = total / rotation
+
       var options = {
         mode: 'text',
         pythonOptions: ['-u'],
         scriptPath: '/home/pi/Desktop/pi2/bioreator-api/scripts',
         args: [port]
       }
-      PythonShell.run('turn_' + mode + '.py', options, function (err, results) {
-        if (err) throw err
-        // results is an array consisting of messages collected during execution
-        console.log('results: %j', results)
-      })
-      res.json({teste: 'teste'})
+      for (var index = 0; index < rotations; index++) {
+        PythonShell.run('turn_' + mode + '.py', options, function (err, results) {
+          if (err) throw err
+          // results is an array consisting of messages collected during execution
+          console.log('results: %j', results)
+        })
+      }
+      res.json({teste: 'Açucar adicionado com sucesso.'})
     } catch (err) {
       res.status(404).json({ error: err.name + ': ' + err.message })
     }
