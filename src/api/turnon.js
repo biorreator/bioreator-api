3d4c0499cbf51dd10e34ccdb4d91db841fe061b6
Alex Cortes <alex.co.alves@gmail.com>
2017-07-04 01:03:51 -0300

    Invertendo on e off na Bomba e Motor

--- src/api/turnon.js
+++ src/api/turnon.js
@@ -8,6 +8,36 @@ export default ({ config, db }) => {
     try {
       var port = body.port
       var mode = body.mode
+
+      var inversePort
+      var inverseMode
+
+      if(mode == 'on'){
+        inverseMode = 'off'
+      }else{
+        inverseMode = 'on'
+      }
+
+      if(port == '17'){
+        inversePort = '22'
+      }else if(port == '22'){
+        inversePort == '17'
+      }
+
+      if((port == '17' || port =='22') && mode == 'on'){
+        var inverseOptions = {
+          mode: 'text',
+          pythonOptions: ['-u'],
+          scriptPath: '/home/pi/Desktop/pi2/bioreator-api/scripts',
+          args: [inversePort]
+        }
+        PythonShell.run('turn_' + inverseMode + '.py', inverseOptions, function (err, results) {
+          if (err) throw err
+          // results is an array consisting of messages collected during execution
+          console.log('results: %j', results)
+        })        
+      }
+
       var options = {
         mode: 'text',
         pythonOptions: ['-u'],
