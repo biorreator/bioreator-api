804cacebf7540ec2b053fbbb2c1f85301e6c3162
Marco Silva <marcowilliamdf@gmail.com>
2017-07-04 22:08:09 -0300

    Change phone token

--- src/helpers/pushnotification.js
+++ src/helpers/pushnotification.js
@@ -1,6 +1,6 @@
 import gcm from 'node-gcm'
 const senderId = 'AAAAQfYxQL4:APA91bGmPm92cwBwL70bp2-2xe-19pLA1kEuuRKoWE-XlTJqZyhFZbBXh9qO_ThLDe9_0bXj0eVvjKUJIlPMPMJiLHeCtQzTaYXTPqt_I9rycbwStVW_Fu0Sl_I_FwwKRyUKFnNfcJ1D'
-const regTokens = ['cUnOJQ9vDOw:APA91bFSBHyWO_bHzqUw04k2bksj77rmv4GAiAqH1tlByONJ6YRmil8-mcTOWXrveVFgQI1xvrDTcaRoK8t7yrIQ-xIfVej91q4Wj0RJek1YUdrMaQ-w2VG0Eb9PuccBpOvw54anEy0g']
+const regTokens = ['cAAuOrCvoc8:APA91bGondHpoRn5VRyY6yXA1BVNoULWAxvgmEUo0cJnZ0mw-BBAH_OjMDyZaEm43SYo7Ee2D8Y-YrSW0Fkg4XU1dY5JKDWKPk-YcSrUied_xb3460KE2ZCUUvl7mT0RHuMbMBxYxRNR']
 
 var sendPush = function (pushMessage) {
   var sender = new gcm.Sender(senderId)
