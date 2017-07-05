f59d5d61abea5bf066ca81553cab69905fdcc4b6
matheus.godinho <matheus.godinho@zrp.com.br>
2017-07-04 23:09:17 -0300

    Atualizando coleta de dados

--- src/helpers/measureRoutine.js
+++ src/helpers/measureRoutine.js
@@ -2,8 +2,9 @@ import cron from 'cron'
 import PythonShell from 'python-shell'
 
 var createNewJob = function (minute, reactionId) {
+  const nextMinute = minute + 1
   const job = new cron.CronJob({
-    cronTime: '00 ' + minute + ' * * * *',
+    cronTime: '00 ' + nextMinute + ' * * * *',
     onTick: async function () {
       var options = {
         mode: 'text',
@@ -15,7 +16,7 @@ var createNewJob = function (minute, reactionId) {
         if (err) {
           console.log(err)
         }
-        console.log("Enviando post de medidas")    
+        console.log("Enviando post de medidas")
       })
     },
     start: false,
